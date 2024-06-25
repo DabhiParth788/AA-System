84f182203251615d36b0d369ee30fee36f690be9
Dabhi Parth <parthdabhi788@gmail.com>
2024-06-25 14:38:01 +0530

    add class details

--- admin/classDetails.php
+++ admin/classDetails.php
@@ -12,4 +12,30 @@
     }
 ?>
 
-// Classes data are available
+<!-- Hoverable Rows -->
+<div class="card">
+<table class="table table-hover table-nowrap mb-0 ">
+    <thead>
+        <tr>
+            <th scope="col">sr no.</th>
+            <th scope="col">Class Name</th>
+            <th scope="col">Teacher Name</th>
+        </tr>
+    </thead>
+    <?php 
+    $sr = 1;
+        if(mysqli_num_rows($result) > 0){
+            foreach($classes as $class){
+                echo "<tr>";
+                echo "<td>".$sr."</td>";
+                echo "<td>".$class[0]."</td>";
+                echo "<td>".$class[1]."</td>";
+                echo "</tr>";
+                $sr++;
+            }     
+        }else{
+            echo "<tr><td colspan='5'>No Data Found</td></tr>";
+        }
+    ?>
+</table>
+</div>
