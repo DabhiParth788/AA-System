d571e51fe798ed15a47dc0565d4f3507f8e868f9
Krish Bhatt <Krishbhatt1217@gmail.com>
2024-06-25 15:57:29 +0530

    view attendance data ready

--- teacher/viewAttendance.php
+++ teacher/viewAttendance.php
@@ -1,3 +1,27 @@
 <?php 
-    
+    if($_GET["date"]){
+    $school = $detail[2];
+    $class = $detail[3];
+
+    $sql = "SELECT enrollment_number, name FROM student WHERE `school` LIKE '$school' AND `class` LIKE '$class'";
+
+    $result = mysqli_query($conn, $sql);
+    $students = array();
+    while($student = mysqli_fetch_assoc($result)){
+        $students[] = array($student["name"], $student["enrollment_number"]);
+    }
+
+    $date = $_GET["date"];
+    $tb = "$school". "_" . "$class";
+    $sql = "SELECT * FROM $tb WHERE date like $date";
+    $result = mysqli_query($conn, $sql);
+    $attendance = array();
+    if($att = mysqli_fetch_assoc($result)){
+        foreach($students as $stu){
+            $temp = array();
+            $temp[] = $att[$stu];
+        }
+        $attendance = $temp;
+    }
+}
 ?>
